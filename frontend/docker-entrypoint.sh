6e5c159b76a7e54fe65b4e113783c731d339e137
Piyush Agrawal <piyushagrawal1403@gmail.com>
2025-08-11 20:30:30 +0530

    added variable to nignx proxy so that it can be ser at deployment time through env

---
+++ frontend/docker-entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+# Replace ${API_URL} in nginx.conf.template with env var value, output to nginx.conf
+envsubst '${API_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
+
+# Start nginx
+exec nginx -g 'daemon off;'
