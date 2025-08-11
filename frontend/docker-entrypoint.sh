c6d225247d55aad87a4276a84d76f03bb86fe677
Piyush Agrawal <piyushagrawal1403@gmail.com>
2025-08-11 20:44:55 +0530

    updated entrypoint.sh

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 
-# Replace ${API_URL} in nginx.conf.template with env var value, output to nginx.conf
-envsubst '${API_URL}' < /etc/nginx/nginx.conf > /etc/nginx/conf.d/default.conf
+# Default to a placeholder if API_URL is not set
+API_URL=${API_URL:-http://localhost:8080}
 
-# Start nginx
-exec nginx -g 'daemon off;'
+# Replace placeholder in the Nginx config
+sed -i "s|__API_URL__|$API_URL|g" /etc/nginx/conf.d/default.conf
+
+echo "Starting Nginx with API_URL=$API_URL..."
+nginx -g "daemon off;"
