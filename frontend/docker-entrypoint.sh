5e788e10670c5f06087640d02119faf8b83a1441
Piyush Agrawal <piyushagrawal1403@gmail.com>
2025-08-11 22:12:35 +0530

    fixed nginx proxy configuration to use env

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
 set -e
 
-# Default to a placeholder if API_URL is not set
-API_URL=${API_URL:-http://localhost:8080}
+# Default to localhost if API_HOST is not set
+API_HOST=${API_HOST:-localhost:8080}
 
-# Replace placeholder in the Nginx config
+# If host contains 'localhost', use http, else https
+if [[ "$API_HOST" == *"localhost"* ]]; then
+    API_URL="http://$API_HOST"
+else
+    API_URL="https://$API_HOST"
+fi
+
+# Replace placeholders in nginx config
 sed -i "s|__API_URL__|$API_URL|g" /etc/nginx/conf.d/default.conf
+sed -i "s|__API_HOST__|$API_HOST|g" /etc/nginx/conf.d/default.conf
 
 echo "Starting Nginx with API_URL=$API_URL..."
 nginx -g "daemon off;"
