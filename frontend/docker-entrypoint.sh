276af61bf4b150ba85215ed197ba68ed28e58fe9
Piyush Agrawal <piyushagrawal1403@gmail.com>
2025-08-11 20:41:31 +0530

    updated entrypoint.sh

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Replace ${API_URL} in nginx.conf.template with env var value, output to nginx.conf
-envsubst '${API_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${API_URL}' < /etc/nginx/nginx.conf > /etc/nginx/conf.d/default.conf
 
 # Start nginx
 exec nginx -g 'daemon off;'
